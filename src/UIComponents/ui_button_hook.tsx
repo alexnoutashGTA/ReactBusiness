db3b42a210ac0e2d87066e0caabec61d5965bcf2
Alex Noutash <alnoutash@hotmail.com>
2025-01-16 00:49:14 -0500

    styled components

--- src/UIComponents/ui_button_hook.tsx
+++ src/UIComponents/ui_button_hook.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from "react";
 import PropTypes from "prop-types";
 import BusinessRuleType from "../BusinessRules/BusinessRuleType";
+import styled from "styled-components";
 
 
 type ButtonAccessibilityProps ={
@@ -25,8 +26,39 @@ function UiButtonHook({accProps, uiProps, businessRules}:ButtonProps) {
 
     }, [accProps, uiProps]);
 
+    const theme = {
+        blue: {
+            default: "#3f51b5",
+            hover: "#283593",
+        },
+        pink: {
+            default: "#e91e63",
+            hover: "#ad1457",
+        },
+    };
+
+    const Button = styled.button`
+  background-color: ${ theme["pink"].default};
+  color: white;
+  padding: 5px 15px;
+  border-radius: 5px;
+  outline: 0;
+  border: 0; 
+  text-transform: uppercase;
+  margin: 10px 0px;
+  cursor: pointer;
+  box-shadow: 0px 2px 2px lightgray;
+  transition: ease background-color 250ms;
+  &:hover {
+    background-color: ${theme["pink"].hover};
+  }
+  &:disabled {
+    cursor: default;
+    opacity: 0.7;
+  }
+`;
     return (
-        <button name={accProps.name}>{uiProps.text}</button>
+        <Button name={accProps.name}>{uiProps.text}</Button>
     )
 }
 
